ffef9861811c341d212d8e8363dc398b8fe7ca2c
Ridhan Ramdhan <rridhan@users.noreply.github.com>
2015-08-20 22:52:38 +0700

    Update plugins-sprknc.js

--- js/plugins-sprknc.js
+++ js/plugins-sprknc.js
@@ -1,1 +1,156 @@
-var searchvisible=0;$("#search-menu").click(function(a){a.preventDefault();var b=$("#search-icon");b.hasClass("ion-ios-search-strong")?(b.addClass("ion-ios-close-empty"),b.removeClass("ion-ios-search-strong")):(b.removeClass("ion-ios-close-empty"),b.addClass("ion-ios-search-strong")),0===searchvisible?($("#search-form").slideLeft(200),$("#s").focus(),searchvisible=1):($("#search-form").slideRight(200),searchvisible=0)}),function(a){"use strict";function b(a){return new RegExp("(^|\\s+)"+a+"(\\s+|$)")}function f(a,b){var f=c(a,b)?e:d;f(a,b)}var c,d,e;"classList"in document.documentElement?(c=function(a,b){return a.classList.contains(b)},d=function(a,b){a.classList.add(b)},e=function(a,b){a.classList.remove(b)}):(c=function(a,c){return b(c).test(a.className)},d=function(a,b){c(a,b)||(a.className=a.className+" "+b)},e=function(a,c){a.className=a.className.replace(b(c)," ")});var g={hasClass:c,addClass:d,removeClass:e,toggleClass:f,has:c,add:d,remove:e,toggle:f};"function"===typeof define&&define.amd?define(g):a.classie=g}(window),$(document).ready(function(){"http://saparakanca-theme.tumblr.com/"!=$("#rcredit").attr("href")&&(window.location.href="http://ridhan.tumblr.com")})();
+var searchvisible = 0;
+
+$("#search-menu").click(function (e) {
+    //This stops the page scrolling to the top on a # link.
+    e.preventDefault();
+
+    var val = $('#search-icon');
+    if (val.hasClass('ion-ios-search-strong')) {
+        val.addClass('ion-ios-close-empty');
+        val.removeClass('ion-ios-search-strong');
+    } else {
+        val.removeClass('ion-ios-close-empty');
+        val.addClass('ion-ios-search-strong');
+    }
+
+
+    if (searchvisible === 0) {
+        //Search is currently hidden. Slide down and show it.
+        $("#search-form").slideLeft(200);
+        $("#s").focus(); //Set focus on the search input field.
+        searchvisible = 1; //Set search visible flag to visible.
+    } else {
+        //Search is currently showing. Slide it back up and hide it.
+        $("#search-form").slideRight(200);
+        searchvisible = 0;
+    }
+});
+
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+(function (window) {
+
+    'use strict';
+
+    // class helper functions from bonzo https://github.com/ded/bonzo
+
+    function classReg(className) {
+        return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+    }
+
+    // classList support for class management
+    // altho to be fair, the api sucks because it won't accept multiple classes at once
+    var hasClass, addClass, removeClass;
+
+    if ('classList' in document.documentElement) {
+        hasClass = function (elem, c) {
+            return elem.classList.contains(c);
+        };
+        addClass = function (elem, c) {
+            elem.classList.add(c);
+        };
+        removeClass = function (elem, c) {
+            elem.classList.remove(c);
+        };
+    } else {
+        hasClass = function (elem, c) {
+            return classReg(c).test(elem.className);
+        };
+        addClass = function (elem, c) {
+            if (!hasClass(elem, c)) {
+                elem.className = elem.className + ' ' + c;
+            }
+        };
+        removeClass = function (elem, c) {
+            elem.className = elem.className.replace(classReg(c), ' ');
+        };
+    }
+
+    function toggleClass(elem, c) {
+        var fn = hasClass(elem, c) ? removeClass : addClass;
+        fn(elem, c);
+    }
+
+    var classie = {
+        // full names
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        // short names
+        has: hasClass,
+        add: addClass,
+        remove: removeClass,
+        toggle: toggleClass
+    };
+
+    // transport
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(classie);
+    } else {
+        // browser global
+        window.classie = classie;
+    }
+
+})(window);
+
+(function () {
+    var triggerBttn = document.getElementById('trigger-overlay'),
+        overlay = document.querySelector('div.overlay'),
+        closeBttn = overlay.querySelector('button.overlay-close');
+    transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+        support = {
+            transitions: Modernizr.csstransitions
+        };
+
+    function toggleOverlay() {
+        if (classie.has(overlay, 'open')) {
+            classie.remove(overlay, 'open');
+            classie.add(overlay, 'close');
+            var onEndTransitionFn = function (ev) {
+                if (support.transitions) {
+                    if (ev.propertyName !== 'visibility') return;
+                    this.removeEventListener(transEndEventName, onEndTransitionFn);
+                }
+                classie.remove(overlay, 'close');
+            };
+            if (support.transitions) {
+                overlay.addEventListener(transEndEventName, onEndTransitionFn);
+            } else {
+                onEndTransitionFn();
+            }
+        } else if (!classie.has(overlay, 'close')) {
+            classie.add(overlay, 'open');
+        }
+    }
+
+    triggerBttn.addEventListener('click', toggleOverlay);
+    closeBttn.addEventListener('click', toggleOverlay);
+})();
+
+//<![CDATA[
+$(document).ready(function () {
+    if ($("#rcredit").attr("href") != "http://saparakanca-theme.tumblr.com/") {
+        window.location.href = "http://ridhan.tumblr.com";
+    }
+});
+//]]>
